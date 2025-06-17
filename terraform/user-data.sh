30d755750c204f0b8f4a74351ab3c26ccf67022a
amitkumar7353 <amitbhavikatte12@gmail.com>
2025-06-17 19:57:14 +0530

    Update user-data.sh

--- terraform/user-data.sh
+++ terraform/user-data.sh
@@ -4,6 +4,6 @@ sudo apt install docker.io -y
 sudo systemctl start docker
 sudo systemctl enable docker
 
-docker pull <your_aws_account_id>.dkr.ecr.ap-south-1.amazonaws.com/strapi-task6-repo:latest
-docker run -d -p 80:1337 <your_aws_account_id>.dkr.ecr.ap-south-1.amazonaws.com/strapi-task6-repo:latest
+docker pull 565827786815.dkr.ecr.ap-south-1.amazonaws.com/strapi-cicd-terraform:latest
+docker run -d -p 80:1337 565827786815.dkr.ecr.ap-south-1.amazonaws.com/strapi-cicd-terraform:latest
 
